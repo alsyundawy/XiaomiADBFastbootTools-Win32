6df235b443d87e56dc05ab8b3a87e7df0ca7f288
Szaki <gergely.szakallas@gmail.com>
2018-04-29 15:39:09 +0200

    Changed hierarchy

---
+++ src/XiaomiADBFastbootTools/AboutDialog.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace XiaomiADBFastbootTools
+{
+    partial class AboutDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.redditLink = new System.Windows.Forms.LinkLabel();
+            this.twitterLink = new System.Windows.Forms.LinkLabel();
+            this.bOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Xiaomi ADB/Fastboot Tools";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Version 2.0.0.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Developed by Saki_EU";
+            // 
+            // redditLink
+            // 
+            this.redditLink.AutoSize = true;
+            this.redditLink.Location = new System.Drawing.Point(69, 88);
+            this.redditLink.Name = "redditLink";
+            this.redditLink.Size = new System.Drawing.Size(124, 17);
+            this.redditLink.TabIndex = 3;
+            this.redditLink.TabStop = true;
+            this.redditLink.Text = "r/Xiaomi on Reddit";
+            this.redditLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.redditLink_LinkClicked);
+            // 
+            // twitterLink
+            // 
+            this.twitterLink.AutoSize = true;
+            this.twitterLink.Location = new System.Drawing.Point(69, 115);
+            this.twitterLink.Name = "twitterLink";
+            this.twitterLink.Size = new System.Drawing.Size(123, 17);
+            this.twitterLink.TabIndex = 4;
+            this.twitterLink.TabStop = true;
+            this.twitterLink.Text = "Saki_EU on Twitter";
+            this.twitterLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.twitterLink_LinkClicked);
+            // 
+            // bOK
+            // 
+            this.bOK.Location = new System.Drawing.Point(95, 141);
+            this.bOK.Name = "bOK";
+            this.bOK.Size = new System.Drawing.Size(75, 23);
+            this.bOK.TabIndex = 5;
+            this.bOK.Text = "OK";
+            this.bOK.UseVisualStyleBackColor = true;
+            this.bOK.Click += new System.EventHandler(this.bOK_Click);
+            // 
+            // AboutDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(267, 181);
+            this.Controls.Add(this.bOK);
+            this.Controls.Add(this.twitterLink);
+            this.Controls.Add(this.redditLink);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel redditLink;
+        private System.Windows.Forms.LinkLabel twitterLink;
+        private System.Windows.Forms.Button bOK;
+    }
+}
---
+++ src/XiaomiADBFastbootTools/CustomAppAdder.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace XiaomiADBFastbootTools
+{
+    partial class CustomAppAdder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbApps = new System.Windows.Forms.TextBox();
+            this.bDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter apps by their package names (one per line):";
+            // 
+            // tbApps
+            // 
+            this.tbApps.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbApps.Location = new System.Drawing.Point(12, 29);
+            this.tbApps.Multiline = true;
+            this.tbApps.Name = "tbApps";
+            this.tbApps.Size = new System.Drawing.Size(311, 256);
+            this.tbApps.TabIndex = 1;
+            // 
+            // bDone
+            // 
+            this.bDone.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bDone.Location = new System.Drawing.Point(12, 291);
+            this.bDone.Name = "bDone";
+            this.bDone.Size = new System.Drawing.Size(311, 28);
+            this.bDone.TabIndex = 2;
+            this.bDone.Text = "Done!";
+            this.bDone.UseVisualStyleBackColor = true;
+            // 
+            // CustomAppAdder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(335, 327);
+            this.Controls.Add(this.bDone);
+            this.Controls.Add(this.tbApps);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Roboto Medium", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CustomAppAdder";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add custom apps";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bDone;
+        public System.Windows.Forms.TextBox tbApps;
+    }
+}
